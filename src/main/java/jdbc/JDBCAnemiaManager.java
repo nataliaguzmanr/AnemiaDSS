75c12b42a1c1a01c92deed3b8216fe9f65073a16
maria-ml <maria.m.labrador@gmail.com>
2024-03-20 16:40:16 +0100

    anemiaManager created but not tested

---
+++ src/main/java/jdbc/JDBCAnemiaManager.java
@@ -0,0 +1,48 @@
+package jdbc;
+
+import diagnosis.Anemia;
+import diagnosis.AnemiaType;
+import diagnosis.Gender;
+import diagnosis.Symptom;
+import ifaces.AnemiaManager;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public class JDBCAnemiaManager implements AnemiaManager {
+
+    private JDBCManager anemiaManager;
+
+    public JDBCAnemiaManager(JDBCManager m) {
+        this.anemiaManager = m;
+    }
+
+    public Anemia getAnemia(Integer anemia_id){
+        Anemia anemia = null;
+
+        try {
+            String sql = " SELECT * FROM Anemia WHERE anemiaTable_id = ?";
+            PreparedStatement prep = anemiaManager.getConnection().prepareStatement(sql);
+            prep.setInt(1, anemia_id);
+            ResultSet rs = prep.executeQuery();
+            if (rs.next()) {
+
+                String string_type = rs.getString("anemia_Type");
+                AnemiaType type = AnemiaType.valueOf(string_type);
+                Integer patient_id = rs.getInt("patient_id");
+
+                anemia = new Anemia(anemia_id,type);
+            }
+            rs.close();
+            prep.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return anemia;
+    }
+
+}
